395002ad96c86b2af5b7f28528a7609fb0557801
Omri Ruvio <omriruvio@gmail.com>
2022-07-12 23:37:51 +0300

    feat: add NewsApi functionality

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,23 +1,26 @@
 import './NewsCard.css';
 import CardLabel from '../CardLabel/CardLabel';
+import { parseDate } from '../../utils/parseDate';
 
-const NewsCard = ({ alt, src, date, header, text, source, link }) => {
+const NewsCard = ({ author, content, title, description, publishedAt, url, urlToImage, source: { id, name } }) => {
   return (
     <li>
-      <article className="news-card">
-        <div className="news-card__image-container">
+      <article className='news-card'>
+        <div className='news-card__image-container'>
           <CardLabel />
-          <img className="news-card__image" src={src} alt={alt}></img>
+          <a href={url}>
+            <img className='news-card__image' src={urlToImage} alt={title}></img>
+          </a>
         </div>
-        <div className="news-card__info">
-          <span className="news-card__date">{date}</span>
-          <h3 className="news-card__header">{header}</h3>
-          <blockquote className="news-card__text" cite={source}>
-            {text}
+        <div className='news-card__info'>
+          <span className='news-card__date'>{parseDate(publishedAt)}</span>
+          <h3 className='news-card__header'>{title}</h3>
+          <blockquote className='news-card__text' cite={name}>
+            {description}
           </blockquote>
         </div>
-        <a className="news-card__ref hover-fade" href={link}>
-          {source}
+        <a className='news-card__ref hover-fade' href={url} target={'_blank'} rel='noreferrer'>
+          {name}
         </a>
       </article>
     </li>
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,16 +1,38 @@
 import './SearchForm.css';
-import { exampleCards } from '../../constants/mockData';
+import { newsApi } from '../../utils/NewsApi';
+import { useState } from 'react';
 
 const SearchForm = ({ buttonText = 'Search', handleSearch }) => {
-  const handleSubmit = (e) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleInput = event => {
+    const { value } = event.target;
+    setSearchQuery(value);
+  };
+
+  const handleSubmit = e => {
     e.preventDefault();
-    handleSearch(exampleCards);
+    newsApi
+      .getNewsByQuery(searchQuery)
+      .then(res => {
+        const { articles } = res;
+        handleSearch(articles);
+      })
+      .catch(err => console.log(err));
+    setSearchQuery('');
   };
 
   return (
-    <form onSubmit={handleSubmit} className="search-form">
-      <input className="search-form__input" type={'search'} placeholder="Enter topic"></input>
-      <button className="search-form__submit" type={'submit'}>
+    <form onSubmit={handleSubmit} className='search-form'>
+      <input
+        onChange={handleInput}
+        value={searchQuery || ''}
+        name='search'
+        className='search-form__input'
+        type={'search'}
+        placeholder='Enter topic'
+      ></input>
+      <button className='search-form__submit' type={'submit'}>
         {buttonText}
       </button>
     </form>
