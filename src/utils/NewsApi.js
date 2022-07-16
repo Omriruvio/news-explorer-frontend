1e59b00bc7f6e2bac8ad126c7706b423ab4b8d40
Omri Ruvio <omriruvio@gmail.com>
2022-07-16 23:06:55 +0300

    fix: correct proxy api query params

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -3,18 +3,19 @@ class NewsApi {
     this._baseUrl = baseUrl;
     this._apiKey = apiKey; /* '2022-07-11'; */
     this._fromOffset = fromOffset;
+    const date = new Date();
+    this._todaysDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    this._fromDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate() - this._fromOffset}`;
+    console.log(this._fromDate);
   }
 
-  _getFromDate = () => {
-    const date = new Date();
-    this._fromDate = `${date.getFullYear()}-${date.getMonth() - this._fromOffset}-${date.getDate() + 1}`;
-  };
+  _handleResponse = (res) => (res.ok ? res.json() : Promise.reject(`Error: ${res.status}`));
 
-  _handleResponse = res => (res.ok ? res.json() : Promise.reject(`Error: ${res.status}`));
-
-  getNewsByQuery = param => {
-    return fetch(`${this._baseUrl}?q=${param}&from=${this._fromDate}&sortedBy=publishedAt&apiKey=${this._apiKey}`).then(this._handleResponse);
+  getNewsByQuery = (param) => {
+    return fetch(
+      `${this._baseUrl}?q=${param}&from=${this._fromDate}&to=${this._todaysDate}&sortedBy=publishedAt&pageSize=100&apiKey=${this._apiKey}`
+    ).then(this._handleResponse);
   };
 }
 
-export const newsApi = new NewsApi('https://newsapi.org/v2/everything', '86ad54dd62e841a587816647ddf6c6d7');
+export const newsApi = new NewsApi('https://nomoreparties.co/news/v2/top-headlines', '86ad54dd62e841a587816647ddf6c6d7');
